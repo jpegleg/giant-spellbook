198b0a06d8227856ce1d09c729d7cf424eecc44d
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-08-15 17:22:32 -0400

    Create disassemble.rs

---
+++ src/disassemble.rs
@@ -0,0 +1,14 @@
+use dismael::disassemble_to_string;
+use std::fs;
+
+pub fn le_dis_to_string(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let binary = fs::read(path)?;
+    let machine_code: Vec<u16> = binary
+        .chunks_exact(2)
+        .map(|c| u16::from_le_bytes([c[0], c[1]]))
+        .collect();
+
+    let assembly_source = disassemble_to_string(&machine_code)?;
+    fs::write("disassembly.asmod", assembly_source)?;
+    Ok(())
+}
