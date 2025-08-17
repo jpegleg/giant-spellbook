dce8d9ab107c227d8c13b40a591ea186244b3d6d
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-08-17 13:11:21 -0400

    Update main.rs
    prep for 0.1.7 release
    + Mach-O in 'researcher' magic byte highlighting
    + migrate disassembly to Captone

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     match first_layer.as_str() {
         "-v" => {
-          println!("{{\"Version\": \"0.1.6\"}}");
+          println!("{{\"Version\": \"0.1.7\"}}");
           process::exit(0)
         },
 
