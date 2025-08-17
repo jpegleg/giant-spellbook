e4a533baee6050c1800bbef6a7f5d4e9fa60521a
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-08-17 04:28:33 -0400

    Update researcher.rs
    expanding tips in help print

--- src/researcher.rs
+++ src/researcher.rs
@@ -34,6 +34,7 @@ pub fn color_map() {
     println!("Tip:");
     println!("  export COLUMNS=64 # the column size is adjusted by the COLUMNS environment variable");
     println!("  press the enter key to print the next line");
+    println!("  enter a number then press enter to jump to that line in the file");
     println!("  use control + c to end the session, or read through the end of the file");
     println!();
 
