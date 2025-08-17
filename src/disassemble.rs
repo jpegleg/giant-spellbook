5a5b99e63aa89d40d11286ff011696c34f853a68
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-08-16 22:45:00 -0400

    Update disassemble.rs
    add segment disassemble function

--- src/disassemble.rs
+++ src/disassemble.rs
@@ -12,3 +12,12 @@ pub fn le_dis_to_string(path: &str) -> Result<(), Box<dyn std::error::Error>> {
     fs::write("disassembly.asmod", assembly_source)?;
     Ok(())
 }
+
+pub fn le_dis_segment(segment: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
+    let machine_code: Vec<u16> = segment
+        .chunks_exact(2)
+        .map(|c| u16::from_le_bytes([c[0], c[1]]))
+        .collect();
+
+    Ok(disassemble_to_string(&machine_code)?)
+}
