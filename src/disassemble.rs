b751e996fef80f6d70fce91184a8e9ed8d729d98
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-08-17 13:01:16 -0400

    Update disassemble.rs
    switch from dismael to capstone disassembly

--- src/disassemble.rs
+++ src/disassemble.rs
@@ -1,23 +1,63 @@
-use dismael::disassemble_to_string;
+use capstone::arch::x86::{ArchMode, ArchSyntax};
+use capstone::prelude::*;
+use std::fmt::Write as _;
 use std::fs;
 
 pub fn le_dis_to_string(path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let binary = fs::read(path)?;
-    let machine_code: Vec<u16> = binary
-        .chunks_exact(2)
-        .map(|c| u16::from_le_bytes([c[0], c[1]]))
-        .collect();
-
-    let assembly_source = disassemble_to_string(&machine_code)?;
-    fs::write("disassembly.asmod", assembly_source)?;
+    let bytes = fs::read(path)?;
+    let asm = capstone_disassemble_to_string(&bytes)?;
+    fs::write("disassembly.asmod", asm)?;
     Ok(())
 }
 
 pub fn le_dis_segment(segment: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
-    let machine_code: Vec<u16> = segment
-        .chunks_exact(2)
-        .map(|c| u16::from_le_bytes([c[0], c[1]]))
-        .collect();
+    capstone_disassemble_to_string(segment)
+}
 
-    Ok(disassemble_to_string(&machine_code)?)
+fn capstone_disassemble_to_string(code: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
+    let mut cs = Capstone::new()
+        .x86()
+        .mode(ArchMode::Mode64)
+        .syntax(ArchSyntax::Intel) 
+        .build()?;
+    cs.set_skipdata(true)?;
+
+    let insns = cs.disasm_all(code, 0x0)?;
+
+    let mut out = String::new();
+    format_instructions(&mut out, &insns)?;
+    Ok(out)
+}
+
+fn format_instructions(out: &mut String, insns: &capstone::Instructions) -> Result<(), std::fmt::Error> {
+    for insn in insns.iter() {
+        write!(out, "{:08x}: ", insn.address())?;
+
+        let bytes = insn.bytes();
+        for (i, b) in bytes.iter().enumerate() {
+            if i > 0 {
+                out.push(' ');
+            }
+            write!(out, "{:02x}", b)?;
+        }
+
+        const BYTES_COL_WIDTH: usize = 24;
+        let bytes_str_len = bytes.len().saturating_mul(3).saturating_sub(1);
+        if bytes_str_len < BYTES_COL_WIDTH {
+            for _ in 0..(BYTES_COL_WIDTH - bytes_str_len) {
+                out.push(' ');
+            }
+        } else {
+            out.push(' ');
+        }
+
+        let mnemonic = insn.mnemonic().unwrap_or("");
+        let op_str = insn.op_str().unwrap_or("");
+        if op_str.is_empty() {
+            writeln!(out, "{mnemonic}")?;
+        } else {
+            writeln!(out, "{mnemonic} {op_str}")?;
+        }
+    }
+    Ok(())
 }
