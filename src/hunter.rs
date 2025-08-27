6abee75e38071c1e0c5d485669392d1a0342ddc9
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-08-27 13:39:57 -0400

    Update hunter.rs
    moving all byte string constants in hunter to modules (added 'misc' module to contain byte items not directly associated with a cve)

--- src/hunter.rs
+++ src/hunter.rs
@@ -4,100 +4,9 @@ use chrono::Utc;
 use std::fmt::Write as _;
 
 mod cve;
+mod misc;
 use cve::*;
-
-const PE_MAGIC: &[u8] = b"PE\0\0";
-const ELF_MAGIC: &[u8] = b"\x7FELF";
-const MACHO_MAGIC_32: &[u8] = &[0xFE, 0xED, 0xFA, 0xCE];
-const MACHO_MAGIC_64: &[u8] = &[0xFE, 0xED, 0xFA, 0xCF];
-const MACHO_CIGAM_32: &[u8] = &[0xCE, 0xFA, 0xED, 0xFE];
-const MACHO_CIGAM_64: &[u8] = &[0xCF, 0xFA, 0xED, 0xFE];
-const OLE_CFB: &[u8] = &[0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1];
-const GZIP_MAGIC: &[u8] = &[0x1F, 0x8B, 0x08];
-const SEVENZ_MAGIC: &[u8] = b"7z\xBC\xAF\x27\x1C";
-const RAR_MAGIC_V4: &[u8] = b"Rar!\x1A\x07\x00";
-const RAR_MAGIC_V5: &[u8] = b"Rar!\x1A\x07\x01\x00";
-const ZIP_MAGIC_LOCAL: &[u8] = &[0x50, 0x4B, 0x03, 0x04];
-const ZIP_MAGIC_CENTRAL: &[u8] = &[0x50, 0x4B, 0x01, 0x02];
-const ZIP_MAGIC_END: &[u8] = &[0x50, 0x4B, 0x05, 0x06];
-const UPX_MAGIC: &[u8] = b"UPX!";
-const UPX0: &[u8] = b"UPX0";
-const UPX1: &[u8] = b"UPX1";
-const ASPACK: &[u8] = b"ASPack";
-const MPRESS: &[u8] = b".MPRESS";
-const THEMIDA: &[u8] = b"Themida";
-const VMPROTECT: &[u8] = b"VMProtect";
-const PETITE: &[u8] = b".petite";
-const ENIGMA: &[u8] = b".enigma";
-const KKRUNCHY: &[u8] = b"kkrunchy";
-const BIN_SH: &[u8] = b"/bin/sh";
-const POWERSHELL: &[u8] = b"pwsh.exe";
-const POWERSHELL2: &[u8] = b"pwsh";
-const CMD: &[u8] = b"cmd.exe";
-const SH: &[u8] = b"/sh ";
-const BASH: &[u8] = b"/bash ";
-const PYTHON: &[u8] = b"python ";
-const PYTHON3: &[u8] = b"python3";
-const PERL: &[u8] = b"/bin/perl ";
-const BIN_BASH: &[u8] = b"/bin/bash";
-const BIN_KSH: &[u8] = b"/bin/ksh";
-const BIN_CSH: &[u8] = b"/bin/csh";
-const BIN_ZSH: &[u8] = b"/bin/zsh";
-const BIN_DASH: &[u8] = b"/bin/dash";
-const BIN_ASH: &[u8] = b"/bin/ash";
-const BIN_FISH: &[u8] = b"/bin/fish";
-const BASE64_PE_PREFIX: &str = "TVqQAA";
-const W_POWERSHELL_EXE: &[u8] = &[
-    b'p',0,b'o',0,b'w',0,b'e',0,b'r',0,b's',0,b'h',0,b'e',0,b'l',0,b'l',0,b'.',0,b'e',0,b'x',0,b'e',0
-];
-const W_IEX: &[u8] = &[b'I',0,b'E',0,b'X',0,b'(',0];
-const W_FROMBASE64: &[u8] = &[
-    b'F',0,b'r',0,b'o',0,b'm',0,b'B',0,b'a',0,b's',0,b'e',0,b'6',0,b'4',0,b'S',0,b't',0,b'r',0,b'i',0,b'n',0,b'g',0,b'(',0
-];
-const W_CMD_EXE: &[u8] = &[b'c',0,b'm',0,b'd',0,b'.',0,b'e',0,b'x',0,b'e',0];
-const W_RUNDLL32: &[u8] = &[
-    b'r',0,b'u',0,b'n',0,b'd',0,b'l',0,b'l',0,b'3',0,b'2',0,b'.',0,b'e',0,b'x',0,b'e',0
-];
-const W_MSHTA: &[u8] = &[b'm',0,b's',0,b'h',0,b't',0,b'a',0,b'.',0,b'e',0,b'x',0,b'e',0];
-const W_REGSVR32: &[u8] = &[
-    b'r',0,b'e',0,b'g',0,b's',0,b'v',0,b'r',0,b'3',0,b'2',0,b'.',0,b'e',0,b'x',0,b'e',0
-];
-const W_WSCRIPT_SHELL: &[u8] = &[
-    b'W',0,b'S',0,b'c',0,b'r',0,b'i',0,b'p',0,b't',0,b'.',0,b'S',0,b'h',0,b'e',0,b'l',0,b'l',0
-];
-const W_SCHTASKS: &[u8] = &[
-    b's',0,b'c',0,b'h',0,b't',0,b'a',0,b's',0,b'k',0,b's',0,b'.',0,b'e',0,b'x',0,b'e',0
-];
-const W_VSSADMIN_DELETE: &[u8] = &[
-    b'v',0,b's',0,b's',0,b'a',0,b'd',0,b'm',0,b'i',0,b'n',0,b' ',0,
-    b'd',0,b'e',0,b'l',0,b'e',0,b't',0,b'e',0,b' ',0,
-    b's',0,b'h',0,b'a',0,b'd',0,b'o',0,b'w',0,b's',0
-];
-const W_WEVTUTIL_CL: &[u8] = &[
-    b'w',0,b'e',0,b'v',0,b't',0,b'u',0,b't',0,b'i',0,b'l',0,b' ',0,b'c',0,b'l',0
-];
-const W_BCDEDIT: &[u8] = &[b'b',0,b'c',0,b'd',0,b'e',0,b'd',0,b'i',0,b't',0];
-const W_WBADMIN_DEL_CAT: &[u8] = &[
-    b'w',0,b'b',0,b'a',0,b'd',0,b'm',0,b'i',0,b'n',0,b' ',0,
-    b'd',0,b'e',0,b'l',0,b'e',0,b't',0,b'e',0,b' ',0,
-    b'c',0,b'a',0,b't',0,b'a',0,b'l',0,b'o',0,b'g',0
-];
-const W_RUN_HKCU: &[u8] = &[
-    b'H',0,b'K',0,b'C',0,b'U',0,b'\\',0,
-    b'S',0,b'o',0,b'f',0,b't',0,b'w',0,b'a',0,b'r',0,b'e',0,b'\\',0,
-    b'M',0,b'i',0,b'c',0,b'r',0,b'o',0,b's',0,b'o',0,b'f',0,b't',0,b'\\',0,
-    b'W',0,b'i',0,b'n',0,b'd',0,b'o',0,b'w',0,b's',0,b'\\',0,
-    b'C',0,b'u',0,b'r',0,b'r',0,b'e',0,b'n',0,b't',0,b'V',0,b'e',0,b'r',0,b's',0,b'i',0,b'o',0,b'n',0,b'\\',0,
-    b'R',0,b'u',0,b'n',0
-];
-const W_RUN_HKLM: &[u8] = &[
-    b'H',0,b'K',0,b'L',0,b'M',0,b'\\',0,
-    b'S',0,b'O',0,b'F',0,b'T',0,b'W',0,b'A',0,b'R',0,b'E',0,b'\\',0,
-    b'M',0,b'i',0,b'c',0,b'r',0,b'o',0,b's',0,b'o',0,b'f',0,b't',0,b'\\',0,
-    b'W',0,b'i',0,b'n',0,b'd',0,b'o',0,b'w',0,b's',0,b'\\',0,
-    b'C',0,b'u',0,b'r',0,b'r',0,b'e',0,b'n',0,b't',0,b'V',0,b'e',0,b'r',0,b's',0,b'i',0,b'o',0,b'n',0,b'\\',0,
-    b'R',0,b'u',0,b'n',0
-];
+use misc::*;
 
 #[derive(Debug, Clone, Copy)]
 pub enum Pattern {
@@ -197,73 +106,75 @@ impl Interesting {
 
         // --- Binary: file format / packers (often used to pack malware) ---
         v.extend([
-            ("pe_magic", Pattern::Bytes(PE_MAGIC)),
-            ("elf_magic", Pattern::Bytes(ELF_MAGIC)),
-            ("macho_magic_32", Pattern::Bytes(MACHO_MAGIC_32)),
-            ("macho_magic_64", Pattern::Bytes(MACHO_MAGIC_64)),
-            ("macho_cigam_32", Pattern::Bytes(MACHO_CIGAM_32)),
-            ("macho_cigam_64", Pattern::Bytes(MACHO_CIGAM_64)),
-            ("ole_cfb_magic", Pattern::Bytes(OLE_CFB)),
-            ("gzip_magic", Pattern::Bytes(GZIP_MAGIC)),
-            ("7z_magic", Pattern::Bytes(SEVENZ_MAGIC)),
-            ("rar_magic_v4", Pattern::Bytes(RAR_MAGIC_V4)),
-            ("rar_magic_v5", Pattern::Bytes(RAR_MAGIC_V5)),
-            ("zip_magic_local", Pattern::Bytes(ZIP_MAGIC_LOCAL)),
-            ("zip_magic_central", Pattern::Bytes(ZIP_MAGIC_CENTRAL)),
-            ("zip_magic_end", Pattern::Bytes(ZIP_MAGIC_END)),
-            ("upx_magic", Pattern::Bytes(UPX_MAGIC)),
-            ("upx_section0", Pattern::Bytes(UPX0)),
-            ("upx_section1", Pattern::Bytes(UPX1)),
-            ("aspack_marker", Pattern::Bytes(ASPACK)),
-            ("mpress_marker", Pattern::Bytes(MPRESS)),
-            ("themida_marker", Pattern::Bytes(THEMIDA)),
-            ("vmprotect_marker", Pattern::Bytes(VMPROTECT)),
-            ("petite_marker", Pattern::Bytes(PETITE)),
-            ("enigma_marker", Pattern::Bytes(ENIGMA)),
-            ("kkrunchy_marker", Pattern::Bytes(KKRUNCHY)),
+            ("pe_magic", Pattern::Bytes(misc_bytes::PE_MAGIC)),
+            ("elf_magic", Pattern::Bytes(misc_bytes::ELF_MAGIC)),
+            ("macho_magic_32", Pattern::Bytes(misc_bytes::MACHO_MAGIC_32)),
+            ("macho_magic_64", Pattern::Bytes(misc_bytes::MACHO_MAGIC_64)),
+            ("macho_cigam_32", Pattern::Bytes(misc_bytes::MACHO_CIGAM_32)),
+            ("macho_cigam_64", Pattern::Bytes(misc_bytes::MACHO_CIGAM_64)),
+            ("ole_cfb_magic", Pattern::Bytes(misc_bytes::OLE_CFB)),
+            ("gzip_magic", Pattern::Bytes(misc_bytes::GZIP_MAGIC)),
+            ("7z_magic", Pattern::Bytes(misc_bytes::SEVENZ_MAGIC)),
+            ("rar_magic_v4", Pattern::Bytes(misc_bytes::RAR_MAGIC_V4)),
+            ("rar_magic_v5", Pattern::Bytes(misc_bytes::RAR_MAGIC_V5)),
+            ("zip_magic_local", Pattern::Bytes(misc_bytes::ZIP_MAGIC_LOCAL)),
+            ("zip_magic_central", Pattern::Bytes(misc_bytes::ZIP_MAGIC_CENTRAL)),
+            ("zip_magic_end", Pattern::Bytes(misc_bytes::ZIP_MAGIC_END)),
+            ("upx_magic", Pattern::Bytes(misc_bytes::UPX_MAGIC)),
+            ("upx_section0", Pattern::Bytes(misc_bytes::UPX0)),
+            ("upx_section1", Pattern::Bytes(misc_bytes::UPX1)),
+            ("aspack_marker", Pattern::Bytes(misc_bytes::ASPACK)),
+            ("mpress_marker", Pattern::Bytes(misc_bytes::MPRESS)),
+            ("themida_marker", Pattern::Bytes(misc_bytes::THEMIDA)),
+            ("vmprotect_marker", Pattern::Bytes(misc_bytes::VMPROTECT)),
+            ("petite_marker", Pattern::Bytes(misc_bytes::PETITE)),
+            ("enigma_marker", Pattern::Bytes(misc_bytes::ENIGMA)),
+            ("kkrunchy_marker", Pattern::Bytes(misc_bytes::KKRUNCHY)),
         ]);
 
         // --- Binary: embedded shell/stager hints ---
         v.extend([
-            ("python_use", Pattern::Bytes(PYTHON)),
-            ("perl_use", Pattern::Bytes(PERL)),
-            ("python3_use", Pattern::Bytes(PYTHON3)),
-            ("powershell_use", Pattern::Bytes(POWERSHELL)),
-            ("powershell_use_(wide)", Pattern::Bytes(POWERSHELL2)),
-            ("cmd_use", Pattern::Bytes(CMD)),
-            ("bash_use", Pattern::Bytes(BASH)),
-            ("sh_use", Pattern::Bytes(SH)),
-            ("bin_sh_use", Pattern::Bytes(BIN_SH)),
-            ("bin_bash_use", Pattern::Bytes(BIN_BASH)),
-            ("bin_ksh_use", Pattern::Bytes(BIN_KSH)),
-            ("bin_csh_use", Pattern::Bytes(BIN_CSH)),
-            ("bin_zsh_use", Pattern::Bytes(BIN_ZSH)),
-            ("bin_dash_use", Pattern::Bytes(BIN_DASH)),
-            ("bin_ash_use", Pattern::Bytes(BIN_ASH)),
-            ("bin_fish_use", Pattern::Bytes(BIN_FISH)),
-            ("base64_pe_prefix", Pattern::Str(BASE64_PE_PREFIX)), // ASCII search
+            ("python_use", Pattern::Bytes(misc_bytes::PYTHON)),
+            ("perl_use", Pattern::Bytes(misc_bytes::PERL)),
+            ("python3_use", Pattern::Bytes(misc_bytes::PYTHON3)),
+            ("powershell_use", Pattern::Bytes(misc_bytes::POWERSHELL)),
+            ("powershell_use_(wide)", Pattern::Bytes(misc_bytes::POWERSHELL2)),
+            ("cmd_use", Pattern::Bytes(misc_bytes::CMD)),
+            ("bash_use", Pattern::Bytes(misc_bytes::BASH)),
+            ("sh_use", Pattern::Bytes(misc_bytes::SH)),
+            ("bin_sh_use", Pattern::Bytes(misc_bytes::BIN_SH)),
+            ("bin_bash_use", Pattern::Bytes(misc_bytes::BIN_BASH)),
+            ("bin_ksh_use", Pattern::Bytes(misc_bytes::BIN_KSH)),
+            ("bin_csh_use", Pattern::Bytes(misc_bytes::BIN_CSH)),
+            ("bin_zsh_use", Pattern::Bytes(misc_bytes::BIN_ZSH)),
+            ("bin_dash_use", Pattern::Bytes(misc_bytes::BIN_DASH)),
+            ("bin_ash_use", Pattern::Bytes(misc_bytes::BIN_ASH)),
+            ("bin_fish_use", Pattern::Bytes(misc_bytes::BIN_FISH)),
+            ("base64_pe_prefix", Pattern::Str(misc_bytes::BASE64_PE_PREFIX)),
         ]);
 
         // --- UTF-16LE (wide) suspicious command markers ---
         v.extend([
-            ("wide_powershell_exe", Pattern::Bytes(W_POWERSHELL_EXE)),
-            ("wide_IEX_call", Pattern::Bytes(W_IEX)),
-            ("wide_from_base64_string", Pattern::Bytes(W_FROMBASE64)),
-            ("wide_cmd_exe", Pattern::Bytes(W_CMD_EXE)),
-            ("wide_rundll32_exe", Pattern::Bytes(W_RUNDLL32)),
-            ("wide_mshta_exe", Pattern::Bytes(W_MSHTA)),
-            ("wide_regsvr32_exe", Pattern::Bytes(W_REGSVR32)),
-            ("wide_wscript_shell", Pattern::Bytes(W_WSCRIPT_SHELL)),
-            ("wide_schtasks_exe", Pattern::Bytes(W_SCHTASKS)),
-            ("wide_vssadmin_delete", Pattern::Bytes(W_VSSADMIN_DELETE)),
-            ("wide_wevtutil_cl", Pattern::Bytes(W_WEVTUTIL_CL)),
-            ("wide_bcdedit", Pattern::Bytes(W_BCDEDIT)),
-            ("wide_wbadmin_delete_catalog", Pattern::Bytes(W_WBADMIN_DEL_CAT)),
-            ("wide_run_key_hkcu", Pattern::Bytes(W_RUN_HKCU)),
-            ("wide_run_key_hklm", Pattern::Bytes(W_RUN_HKLM)),
+            ("wide_powershell_exe", Pattern::Bytes(misc_bytes::W_POWERSHELL_EXE)),
+            ("wide_IEX_call", Pattern::Bytes(misc_bytes::W_IEX)),
+            ("wide_from_base64_string", Pattern::Bytes(misc_bytes::W_FROMBASE64)),
+            ("wide_cmd_exe", Pattern::Bytes(misc_bytes::W_CMD_EXE)),
+            ("wide_rundll32_exe", Pattern::Bytes(misc_bytes::W_RUNDLL32)),
+            ("wide_mshta_exe", Pattern::Bytes(misc_bytes::W_MSHTA)),
+            ("wide_regsvr32_exe", Pattern::Bytes(misc_bytes::W_REGSVR32)),
+            ("wide_wscript_shell", Pattern::Bytes(misc_bytes::W_WSCRIPT_SHELL)),
+            ("wide_schtasks_exe", Pattern::Bytes(misc_bytes::W_SCHTASKS)),
+            ("wide_vssadmin_delete", Pattern::Bytes(misc_bytes::W_VSSADMIN_DELETE)),
+            ("wide_wevtutil_cl", Pattern::Bytes(misc_bytes::W_WEVTUTIL_CL)),
+            ("wide_bcdedit", Pattern::Bytes(misc_bytes::W_BCDEDIT)),
+            ("wide_wbadmin_delete_catalog", Pattern::Bytes(misc_bytes::W_WBADMIN_DEL_CAT)),
+            ("wide_run_key_hkcu", Pattern::Bytes(misc_bytes::W_RUN_HKCU)),
+            ("wide_run_key_hklm", Pattern::Bytes(misc_bytes::W_RUN_HKLM)),
 
             // ---- Reverse shells / remote exec ----
-            ("bash_reverse", Pattern::Str("bash -i >& /dev/tcp/")),
+            ("bash_reverse_1", Pattern::Str("bash -i >& /dev/tcp/")),
+            ("bash_reverse_2", Pattern::Str("bash -l > /dev/tcp")),
+            ("bash_reverse_3", Pattern::Str("bash -l >/dev/tcp")),
             ("python_reverse", Pattern::Str("import socket,subprocess,os;")),
             ("php_reverse", Pattern::Str("php -r '$sock=fsockopen(")),
             ("perl_reverse", Pattern::Str("perl -e 'use Socket;")),
