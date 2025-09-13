ec3761361caf0bb49584fc46588c23da0c605d8d
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-13 15:09:51 -0400

    Update main.rs
    version printing update for 0.3.7

--- src/main.rs
+++ src/main.rs
@@ -87,7 +87,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     match first_layer.as_str() {
         "-v" | "--version" => {
-          println!("{{\"Version\": \"0.3.6\"}}");
+          println!("{{\"Version\": \"0.3.7\"}}");
           Ok(())
         },
 
