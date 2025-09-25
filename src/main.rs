321c3e0f0fb01ce32c18cf8a9291edab8adc9ebf
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-09-24 21:07:18 -0400

    Update main.rs
    version printing bump for 0.1.12 release

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     match first_layer.as_str() {
         "-v" => {
-          println!("{{\"Version\": \"0.1.11\"}}");
+          println!("{{\"Version\": \"0.1.12\"}}");
           process::exit(0)
         },
 
