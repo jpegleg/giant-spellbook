0e52f96a1db59d968481bae1704ec02414516730
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-12-14 17:25:04 -0500

    version printing update in prep for 0.1.16 release

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     match first_layer.as_str() {
         "-v" => {
-          println!("{{\"Version\": \"0.1.15\"}}");
+          println!("{{\"Version\": \"0.1.16\"}}");
           process::exit(0)
         },
 
