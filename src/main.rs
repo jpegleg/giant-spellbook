2df88b659cde6679bb0c8b56858c97c3527af3dc
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-01-29 14:12:18 -0500

    version printing update for 0.4.6

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     match first_layer.as_str() {
         "-v" | "--version" => {
-          println!("{{\"Version\": \"0.4.5\"}}");
+          println!("{{\"Version\": \"0.4.6\"}}");
           Ok(())
         },
 
