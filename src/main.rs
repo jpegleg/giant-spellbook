dbb15ea9ed4ec2590d9c51fac6295bbd281440ec
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-08-29 14:21:54 -0400

    v0.1.21 release prep

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     match first_layer.as_str() {
         "-v" => {
-          println!("{{\"Version\": \"0.1.20\"}}");
+          println!("{{\"Version\": \"0.1.21\"}}");
           process::exit(0)
         },
 
