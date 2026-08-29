0d823d0181ee44649ab9ae32a17223446552af72
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-08-29 14:47:23 -0400

    v0.3.23 release prep

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     match first_layer.as_str() {
         "-v" | "--version" => {
-          println!("{{\"Version\": \"0.3.22\"}}");
+          println!("{{\"Version\": \"0.3.23\"}}");
           Ok(())
         },
 
